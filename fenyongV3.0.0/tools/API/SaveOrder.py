2a30b473ce6669c2dfc9c29168c83d7c34d6484d
chunshan160 <1605936478@qq.com>
2020-09-10 08:43:56 +0800

    no message

--- fenyongV3.0.0/tools/API/SaveOrder.py
+++ fenyongV3.0.0/tools/API/SaveOrder.py
@@ -8,19 +8,19 @@ from tools.http_request import HttpRequest
 
 
 def SaveOrder(surroundings, addressId, productStockId, cookies):
+    SaveOrder_headers = {"login": ""}
+    # 获取商品类型
+    type_url = 'http://m.test.hobay.com.cn/ribbon-api/batchCart/immediatelyBuySettlement'
+    type_data = {"productStockIdAndNums": [{"num": 1, "productStockId": 362853, "productId": 1080304}]}
+    type_res = HttpRequest().http_request(type_url, "post", json=type_data,
+                                          cookies=cookies,
+                                          headers=SaveOrder_headers)
+    type = type_res.json()['data']['type']
+
     # 提交订单
     SaveOrder_url = f'http://m.{surroundings}.hobay.com.cn/ribbon-api/batchOrders/immediatelySaveOrder'
-    if
     SaveOrder_data = {"message": "", "couponUserId": "", "addressId": addressId,
-                      "productStockIdAndNums": [{"num": 1, "productStockId": productStockId}], "type": 1}
-
-    SaveOrder_data = {"message": "", "couponUserId": "", "addressId": addressId,
-                      "productStockIdAndNums": [{"num": 1, "productStockId": productStockId}], "type": 2}
-
-    SaveOrder_data = {"message": "", "couponUserId": "", "addressId": addressId,
-                      "productStockIdAndNums": [{"num": 1, "productStockId": productStockId}], "type": 3}
-
-    SaveOrder_headers = {"login": ""}
+                      "productStockIdAndNums": [{"num": 1, "productStockId": productStockId}], "type": type}
     SaveOrder_res = HttpRequest().http_request(SaveOrder_url, "post", json=SaveOrder_data,
                                                cookies=cookies,
                                                headers=SaveOrder_headers)
