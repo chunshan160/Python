e5fabcb0d3f8982e5e3078649af3cbdd0ed30646
chunshan160 <1605936478@qq.com>
2020-09-25 17:53:51 +0800

    no message

---
+++ Hobay/dada.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time :2020/9/25 16:25
+# @Author :春衫
+# @File :dada.py
+
+a=[1,2,3]
+b=[i for i in a]
+print(b)
---
+++ home_work/wuliao.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time :2020/9/25 14:00
+# @Author :春衫
+# @File :wuliao.py
+
+# sum=0
+# for i in range(1,101):
+#     sum=sum+i
+#
+# print(sum)
+
+a = [1, 2, 3, 4, 5]
+
+print(a[:])
+print(a[:-1])
+print(a[0:])
+print(a[:100])
+print(a[-1:-4:-1])
